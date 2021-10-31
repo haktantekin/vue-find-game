4bc5859f7f79c6519d42059597fe21d31bc27534
haktantekin <47484364+haktantekin@users.noreply.github.com>
2021-10-31 14:21:52 +0300

    Created find card game

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,67 @@
 <template>
+  <transition-group name="slideContainer" mode="out-in">
+    <component
+      key="mainComponent"
+      @activeComponentEvent="activeComponent = $event"
+      :is="activeComponent"
+    >
+    </component>
+    <canvas id="canvas" key="canvas" v-show="activeComponent == 'app-celebrate' "></canvas>
+  </transition-group>
 </template>
+
 <script>
-export default {}
+  import GameCards from './components/GameCards'
+  import Failure from './components/Failure'
+  import Celebrate from './components/Celebrate'
+  export default {
+    data(){
+        return{
+            activeComponent: "app-game-cards"
+        }
+    },
+    components :{
+        appGameCards: GameCards,
+        appCelebrate: Celebrate,
+        appFailure: Failure
+    }
+  }
 </script>
-<style></style>
+
+<style>
+  body{
+      font-family: sans-serif;
+  }
+  .slideContainer-enter{}
+  .slideContainer-enter-active{
+  animation: slide-in .3s ease-out forwards;
+  }
+  .slideContainer-leave{}
+  .slideContainer-leave-active{
+  animation: slide-out .3s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from{
+      transform: translateX(-1000px);
+      opacity: 0;
+    }
+    to{
+      transform: translateX(0);
+      opacity: 1;
+
+    }
+  }
+  @keyframes slide-out {
+    from{
+      transform: translateX(0);
+      opacity: 1;
+
+    }
+    to{
+      transform: translateX(1000px);
+      opacity: 0;
+
+    }
+  }
+</style>
